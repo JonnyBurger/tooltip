0fcd4efeb1f3cc94f29c61a499edfead85536a9a
Jonny Burger <jonathanburger11@gmail.com>
2019-05-16 17:09:19 +0200

    more props to popover

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -2,23 +2,47 @@ import React from 'react';
 import {Manager, Reference, Popper} from 'react-popper';
 import {Placement} from 'popper.js';
 import PopupContent from './PopupContent';
+import {LineStyle} from 'csstype';
+import {DivProps} from 'types';
 
 export default ({
 	children,
 	preferredPlacement,
 	onClose,
-	message
+	message,
+	borderWidth,
+	borderColor,
+	borderStyle,
+	arrowSize,
+	popupProps,
+	arrowDivProps
 }: {
 	children: any;
 	preferredPlacement: Placement;
 	onClose: () => void;
 	message: any;
+	borderWidth?: number;
+	borderColor?: string;
+	borderStyle?: LineStyle;
+	arrowSize?: number;
+	popupProps?: DivProps;
+	arrowDivProps?: DivProps;
 }) => (
 	<Manager>
 		<Reference>{({ref}) => React.cloneElement(children, {ref})}</Reference>
 		<Popper placement={preferredPlacement}>
-			{({ref, ...popupProps}) => (
-				<PopupContent passRef={ref} {...popupProps} onClose={onClose}>
+			{({ref, ...popperProps}) => (
+				<PopupContent
+					passRef={ref}
+					{...popperProps}
+					borderWidth={borderWidth}
+					borderStyle={borderStyle}
+					borderColor={borderColor}
+					onClose={onClose}
+					arrowSize={arrowSize}
+					arrowDivProps={arrowDivProps}
+					popupProps={popupProps}
+				>
 					{message}
 				</PopupContent>
 			)}
